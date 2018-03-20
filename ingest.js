873ab4289cbbc801fc9ce1ba367facb980eb33b7
Jason <jason@astucemedia.com>
2018-03-20 16:54:43 -0400

    bug fix

--- ingest.js
+++ ingest.js
@@ -112,19 +112,19 @@ const toUTC = (date) => {
 }
 
 const compactMatch = (match) => ({
-  mid: match.id,
-  k: match.kills,
-  p: match.playlist,
-  t1: match.top1,
-  t3: match.top3,
-  t5: match.top5,
-  t6: match.top6,
-  t10: match.top10,
-  t12: match.top12,
-  t25: match.top25,
-  s: match.score,
-  m: match.minutesPlayed,
-  d: toUTC(new Date(match.dateCollected)).toISOString(),
-  r: match.trnRating,
-  c: match.trnRatingChange
+  mid: match.id || 0,
+  k: match.kills || 0,
+  p: match.playlist || 0,
+  t1: match.top1 || 0,
+  t3: match.top3 || 0,
+  t5: match.top5 || 0,
+  t6: match.top6 || 0,
+  t10: match.top10 || 0,
+  t12: match.top12 || 0,
+  t25: match.top25 || 0,
+  s: match.score || 0,
+  m: match.minutesPlayed || 0,
+  d: toUTC(new Date(match.dateCollected)).toISOString() || 0,
+  r: match.trnRating || 0,
+  c: match.trnRatingChange || 0
 })
