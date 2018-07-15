ebd488af83da7a06b479a7cdf08dcfee093ad695
Jason <rametta@outlook.com>
2018-07-15 09:40:19 -0400

    memory management

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 const { fetchUserData } = require('./fetchUserData')
 const { processUserData } = require('./processUserData')
-const { db } = require('./initFirebase')
+const { db, app } = require('./initFirebase')
 
-const app = () => {
+const io = () => {
   db.ref('/users').once('value', (snap) => {
-    const usersMap = snap.val()
-    const users = Object.values(usersMap)
+    const users = Object.values(snap.val())
+
     users.map((user, i) => {
       setTimeout(() => {
         fetchUserData(user)
-          .then(({ data }) => data.error === undefined && processUserData(data))
+          .then(({ data }) => {
+            if (data.error === undefined) {
+              const { dataWDate, matches } = processUserData(data)
+
+              const updates = matches.reduce((acc, m) => {
+                acc[`/matches/${data.epicUserHandle}/${m.mid}`] = m
+                return acc
+              }, {})
+
+              updates[`/data/${dataWDate.epicUserHandle}`] = dataWDate
+
+              db.ref('/')
+                .update(updates)
+                .then(() => {
+                  console.log(`${data.epicUserHandle} updated`)
+
+                  if (i + 1 === users.length) {
+                    console.log(`Completed ${i + 1}/${users.length} - Exiting`)
+                    app.delete()
+                  }
+                })
+                .catch(() => {
+                  console.log(`${data.epicUserHandle} error`)
+                  app.delete()
+                })
+            }
+          })
           .catch((err) => console.error(err))
       }, 2000 * (i + 1))
     })
   })
 }
 
-app()
+io()
--- src/processUserData.js
+++ src/processUserData.js
@@ -1,25 +1,12 @@
 const moment = require('moment')
-const { db } = require('./initFirebase')
 const { formatMatch } = require('./formatMatch')
 
 const processUserData = (data) => {
-  const { epicUserHandle, recentMatches } = data
-  data.fetchTime = moment.utc().format()
-
-  recentMatches.map((m) => {
-    db.ref(`/matches/${epicUserHandle}/${m.id}`).set(formatMatch(m))
-  })
-
-  db.ref(`/data/${epicUserHandle}`)
-    .set(data)
-    .then(
-      () =>
-        console.log(`${epicUserHandle} processed - ${moment().format('llll')}`),
-      () =>
-        console.error(
-          `${epicUserHandle} failed to process - ${moment().format('llll')}`
-        )
-    )
+  const { recentMatches } = data
+  return {
+    dataWDate: { ...data, fetchTime: moment.utc().format() },
+    matches: recentMatches.map(formatMatch)
+  }
 }
 
 module.exports = { processUserData }
